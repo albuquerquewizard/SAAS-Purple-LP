876dc74be994089e91852599644e846f7625515e
Himanshu <himanshuwankhadegeneral@gmail.com>
2025-05-17 16:56:45 +0530

    Features Section

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,3 +1,7 @@
+import EcosystemIcon from "../assets/icons/ecosystem.svg";
+import GoalIcon from "../assets/icons/goal.svg";
+import SecureIcon from "../assets/icons/secure.svg";
+
 const features = [
   {
     title: "Integration Ecosystem",
@@ -17,5 +21,29 @@ const features = [
 ];
 
 export const Features = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-5xl sm:text-6xl text-center font-bold tracking-tighter">Everything you need</h2>
+        <div className = "max-w-xl mx-auto">
+          <p className="text-white/70 text-center mt-5 text-xl">
+          Enjoy customizable lists, team work tools, and smart tracking all in one place. 
+          Set tasks, get reminders, and see your progress simply and quickly.
+        </p>
+        </div>
+        <div className = "mt-16 sm:flex-row flex flex-col gap-4">
+          {features.map(({ title, description }) => (
+            <div key={title} className = "border border-white/30 px-5 py-10 sm:flex-1 text-center rounded-xl ">
+              <div className = "inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
+                <EcosystemIcon />
+              </div>
+
+              <h3 className= "mt-6 font-bold">{title}</h3>
+              <p className = "mt-2 text-white/70">{description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
